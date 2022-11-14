b6da95406eb957780717b058f87611ce56d18b50
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-14 10:48:55 -0700

    adding conditional rendering

--- src/App.js
+++ src/App.js
@@ -83,6 +83,21 @@ import { useState } from 'react'
 //   )
 // }
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
@@ -104,7 +119,7 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {right}
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
