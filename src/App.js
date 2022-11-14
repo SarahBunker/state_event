7f80194e837f43807ba67528c37f5d5d88bc21cb
Sarah Bunker <ChemEnCoder@gmail.com>
2022-11-14 10:07:42 -0700

    app with Display, button w/ handlers, and app components

--- src/App.js
+++ src/App.js
@@ -33,22 +33,52 @@
 
 import { useState } from 'react'
 
+// const App = () => {
+//   const [ counter, setCounter ] = useState(0)
+//
+//   const increaseByOne = () => setCounter(counter + 1)
+//
+//   const setToZero = () => setCounter(0)
+//
+//   return (
+//     <div>
+//       <div>{counter}</div>
+//       <button onClick={increaseByOne}>
+//         plus
+//       </button>
+//       <button onClick={setToZero}>
+//         zero
+//       </button>
+//     </div>
+//   )
+// }
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
-
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
     </div>
   )
 }
